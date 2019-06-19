5540404ee2683a42d94b8849e3e23c11709c3db1
Renée Cruz <reneebasscruz@gmail.com>
2019-06-19 03:52:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = ""
  
   name_hash.collect do |key, value|
-    if value <= name_hash[:key]  && lowest_value <= value
+    if value <= lowest_value 
       lowest_value = value
       lowest_key = key
     end
