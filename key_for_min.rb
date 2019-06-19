080ef4a85785f4f06e320e3f95c201c5cb07c919
Renée Cruz <reneebasscruz@gmail.com>
2019-06-19 04:02:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     if value <= lowest_value 
       lowest_value = value
       lowest_key = key
-    elsif name_hash = {} 
+    else name_hash = {} 
     return nil
     end
   end
